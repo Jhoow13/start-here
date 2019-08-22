cc6cb8bc032f2af89843f9329c4ac57b50d7f3d9
Joel Silva <joel.caique@superaremedia.com.br>
2019-08-22 16:21:47 -0300

    Refactor boilerplate

--- README.md
+++ README.md
@@ -2,46 +2,4 @@
 
 This is a simple boilerplate for setting up my projects.
 
-## How to start
-
-You'll need NodeJS and Gulp to fire up a localhost server that will automatically restart every change you make on the files. Browser-Sync helps doing this magic.
-
-## Folders and files structure
-
-- assets
-  - images, font files and assets in general
-- scripts
-  - js files
-- styles
-  - less files
-- src
-  - where the compiled files will be
-- .editorconfig
-- .eslintrc.json
-- .gitignore
-- gulpfile.js
-- index.html
-- package.json
-- README.md
-
-
-## npm scripts
-
-- **npm run lint** - runs [ESLint][1] using the [Airbnb][2] styleguide to check errors on js files.
-
-## Gulp tasks
-
-On the gulpfile you'll find the folders and files on the *appFiles* object. Any new folder or major change in the structure you have to put there.
-
-- **gulp clean** - Remove the files on the src folder.
-- **gulp css** - Compile .less files, concat in *style.css* and send to the src folder.
-- **gulp js** - Concat js files in *script.js* and send to the src folder.
-- **gulp reload** - auto reload the browser
-- **gulp serve** - Run the css and js tasks, fire up a server with browsersync on the root directory. Auto restart on changes in the css and js files.
-- **gulp** - Run the serve task by default.
-
-
 > *My workflow is in development and might have some changes in the future*
-
-[1]: [https://eslint.org/]
-[2]: [https://github.com/airbnb/javascript]
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,74 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var less = require('gulp-less');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    cssNano = require('gulp-cssnano');
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    imageMin = require('gulp-imagemin');
 
-var appFiles = {
-    cssFiles:[
-        'styles/*.less'
+var files = {
+    pugFiles:[
+      'src/index.pug'
+    ],
+    scssFiles:[
+      'src/sass/all.scss'
     ],
     jsFiles:[
-        'scripts/main.js'
+      'src/js/*.js'
+    ],
+    imgFiles:[
+      'src/img/**/*.*',
     ]
 };
 
 gulp.task('clean', function(){
-    return gulp.src('src/*')
+    return gulp.src('dist/*')
     .pipe(clean());
 });
 
-gulp.task('css', function(){
-    return gulp.src(appFiles.cssFiles)
-    .pipe(less())
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('src'));
+gulp.task('pug', function(){
+  return gulp.src(files.pugFiles)
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('css-dev', function(){
+    return gulp.src(files.scssFiles)
+    .pipe(sourcemaps.init())
+    .pipe(concat('style.min.css'))
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssNano())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('css-prod', function(){
+  return gulp.src(files.scssFiles)
+  .pipe(concat('style.min.css'))
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(cssNano())
+  .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('js', function(){
-    return gulp.src(appFiles.jsFiles)
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest('src'));
+    return gulp.src(files.jsFiles)
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('img', function() {
+	gulp.src(files.imgFiles)
+		.pipe(imageMin())
+		.pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('reload', function(done){
@@ -36,16 +76,17 @@ gulp.task('reload', function(done){
     done();
 });
 
-gulp.task('serve', ['css','js'], function(){
+gulp.task('dev', ['pug','img', 'css-dev','js'], function(){
   browserSync.init({
     server: {
-        baseDir: "./"
+        baseDir: "./dist/"
     }
   });
 
-  gulp.watch('index.html', ['reload']);
-  gulp.watch(appFiles.cssFiles, ['css', 'reload']);
-  gulp.watch(appFiles.jsFiles, ['js', 'reload']);
+  gulp.watch([files.pugFiles, 'src/pug/**/*.pug'], ['pug', browserSync.reload]);
+  gulp.watch([files.scssFiles, 'src/sass/**/*.scss'], ['css-dev', browserSync.reload]);
+  gulp.watch([files.jsFiles, 'src/js/**/*.js'], ['js', browserSync.reload]);
 });
 
-gulp.task('default',['serve']);
+gulp.task('default', ['dev']);
+gulp.task('prod', ['pug', 'img', 'css-prod', 'js']);
